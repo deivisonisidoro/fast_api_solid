88413230752227ada718103236ab7db538c74870
Deivison Isidoro da Silva <61429017+deivisonisidoro@users.noreply.github.com>
2023-02-18 13:19:56 -0300

    feat: creating router for creating user

--- src/server.py
+++ src/server.py
@@ -1,8 +1,22 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, HTTPException, status
+from sqlalchemy.orm import Session
+
+from src.config.database import get_db
+from src.repositories.user_repository import UserRepository
+from src.schemas.user_schema import UserCreate, UserOut
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.post("/user/create", status_code=status.HTTP_201_CREATED, response_model=UserOut)
+async def create_user(user: UserCreate, db: Session = Depends(get_db)):
+    user_service = UserRepository(db)
+    db_user = user_service.get_user_by_email(user.email)
+
+    if db_user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Email already registered",
+        )
+
+    return user_service.create_user(user)
