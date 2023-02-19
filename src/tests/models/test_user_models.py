e3c241e398848cb6f70ed5f5cf836861aa8ff255
Deivison Isidoro da Silva <61429017+deivisonisidoro@users.noreply.github.com>
2023-02-19 12:03:27 -0300

    test: improving test of model

--- src/tests/models/test_user_models.py
+++ src/tests/models/test_user_models.py
@@ -1,33 +1,34 @@
 import pytest
 from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm import Session
 
 from src.models import User
 
 
 class TestUserModel:
-    def test_create_user(self, db):
+    def test_create_user(self, db: Session, user_data: dict):
         # test
-        user = User(name="Test User", email="test@example.com", password="password")
+        user = User(name=user_data["name"], email=user_data["email"], password=user_data["password"])
         db.add(user)
         db.commit()
 
         # assert
         assert user.id is not None
-        assert user.name == "Test User"
-        assert user.email == "test@example.com"
-        assert user.password == "password"
+        assert user.name == user_data["name"]
+        assert user.email == user_data["email"]
+        assert user.password == user_data["password"]
 
         # cleanup
         db.rollback()
 
-    def test_create_duplicate_user(self, db):
+    def test_create_duplicate_user(self, db: Session, user_data: dict):
         # create a user
-        user = User(name="Test User", email="testuser@example.com", password="password")
+        user = User(name=user_data["name"], email=user_data["email"], password=user_data["password"])
         db.add(user)
         db.commit()
 
         # attempt to create a user with the same email
         with pytest.raises(IntegrityError):
-            user2 = User(name="Test User 2", email="testuser@example.com", password="password")
+            user2 = User(name=user_data["name"], email=user_data["email"], password=user_data["password"])
             db.add(user2)
             db.commit()
