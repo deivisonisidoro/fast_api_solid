895daafb42aa251c485561836ff8447343a413a0
Deivison Isidoro da Silva <61429017+deivisonisidoro@users.noreply.github.com>
2023-02-21 09:12:34 -0300

    feat: creating a new router for authentication

--- src/routers.py
+++ src/routers.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 
+from src.controllers import auth_controller as auth
 from src.controllers import user_controller as user
 
 router = APIRouter()
 
 
 router.include_router(user.router, prefix="/users", tags=["User"])
+router.include_router(auth.router, prefix="/auth", tags=["Authentication"])
