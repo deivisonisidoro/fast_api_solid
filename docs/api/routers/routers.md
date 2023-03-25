a81429ffcbbe09673c3455a692492ef2dc1e2622
Deivison Isidoro da Silva <61429017+deivisonisidoro@users.noreply.github.com>
2023-03-25 11:33:15 -0300

    docs: adding the routers interface in my documentation

---
+++ docs/api/routers/routers.md
@@ -0,0 +1,3 @@
+# Routers
+
+::: src.routers.router
--- src/routers/router.py
+++ src/routers/router.py
@@ -1,3 +1,25 @@
+"""
+A module that defines an APIRouter instance and registers various routes.
+
+This module imports two modules: auth_routers and user_routers from the src.routers package.
+These modules define the authentication and user routes respectively.
+
+Attributes:
+    router (APIRouter): An instance of the APIRouter class provided by FastAPI.
+
+Examples:
+    To include the user routes, you can call the `include_router` method on the router instance:
+    
+    >>> from src.api import router
+    >>> from src.routers.user_routers import router as user_router
+    >>> router.include_router(user_router, prefix="/users", tags=["User"])
+
+    To include the authentication routes, you can call the `include_router` method on the router instance:
+    
+    >>> from src.api import router
+    >>> from src.routers.auth_routers import router as auth_router
+    >>> router.include_router(auth_router, prefix="/auth", tags=["Authentication"])
+"""
 from fastapi import APIRouter
 
 from src.routers import auth_routers as auth
